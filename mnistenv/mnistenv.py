e88e0441b529939b7435aae27e712349e20bdc4b
tehZevo <tehzevo@users.noreply.github.com>
2019-09-10 14:25:40 -0400

    Add step limit

--- mnistenv/mnistenv.py
+++ mnistenv/mnistenv.py
@@ -7,11 +7,13 @@ from keras.datasets import mnist
 #TODO: implement seed
 
 class MnistEnv(gym.Env):
-  def __init__(self, blackout=False):
+  def __init__(self, blackout=False, max_episode_steps=200):
     (x_train, y_train), (x_test, y_test) = mnist.load_data()
     self.x_train = x_train / 255.0
     self.y_train = y_train
 
+    self.steps = 0
+    self.max_episode_steps = max_episode_steps
     self.blackout = blackout
 
     #TODO: dont force 1d
@@ -29,11 +31,18 @@ class MnistEnv(gym.Env):
       reward = 1
       done = True
 
+    self.steps += 1
+
+    if self.steps > self.max_episode_steps:
+      done = True
+
     return obs, reward, done, {}
 
   def reset(self):
     i = np.random.randint(len(self.x_train))
     self.x = self.x_train[i].flatten() #TODO: dont force 1d
     self.y = self.y_train[i]
+
+    self.steps = 0
 
     return self.x
